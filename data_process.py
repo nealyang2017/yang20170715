f8414c363465abf685310bb2dd821d849b2938db
neal <neal@neal.local>
2017-07-16 14:09:24 +0800

    add log data process

--- data_process.py
+++ data_process.py
@@ -2,6 +2,8 @@
 """
 Functions for data recording.
 """
+import matplotlib.pyplot as plt
+import re
 
 def record_pressure_data(file_name, pressure_data, speed_data):
     file1 = open('Recorded_Data/' + file_name + "_pressure" + '.tsv', 'a')
@@ -20,4 +22,25 @@ def record_pressure_data(file_name, pressure_data, speed_data):
 def record_intentional_angle(file_name, angle_data):
     file = open('Recorded_Data/' + file_name + '.tsv', 'a')
     file.write(str(angle_data) + '\n')
+    file.close()
+    
+def log_to_path(file_name):
+    """Write in path log data, produce a tsv data point file"""
+    data_x = []
+    data_y = []
+    f = open('Recorded_Data/' + file_name + '.txt', 'w')
+    line_index = 0
+    for line in f:
+        if "LE:0" in line:
+            if line_index %  8== 0:
+                pattern = re.compile(r'(\[.*?\])')
+                match = re.findall(pattern, line)[0][1:-1].split(',')
+                data_x.append(float(match[0]))
+                data_y.append(float(match[1]))
+            line_index += 1
+    file = open('Recorded_Data/' + file_name + '.tsv', 'w')
+    for i in range(len(data_x)):
+        file.write(str(data_x[i]) + '\t')
+        file.write(str(data_y[i]) + '\t')
+        file.write('\n')
     file.close()
